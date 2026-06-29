948f7252a82f2a169f31bc99998b4d9d06dd021b
Marco Mendão <marco.mendao@betacode.tech>
2026-06-29 08:07:02 +0100

    Add fix on probabilities

--- lib/simulation/actions.ts
+++ lib/simulation/actions.ts
@@ -9,9 +9,9 @@ export function chooseOptionalAction(
   personality: Personality,
 ): ActionType | null {
   const result = weightedRandom<OptionalRoll>({
-    post: 20 + personality.stats.creativity * 2,
-    lurk: 30,
-    skip: 50,
+    post: 8 + personality.stats.creativity * 0.5,
+    lurk: 40,
+    skip: 70,
   });
 
   return result === "skip" ? null : result;
